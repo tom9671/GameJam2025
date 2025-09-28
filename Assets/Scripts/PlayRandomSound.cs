dee04c865deffd58e232f77c740e43574b9ab026
tom9671 <timothymarroquin03@yahoo.com>
2025-09-28 16:43:40 -0500

    Implemented new art assets and relocated certain buttons

---
+++ Assets/Scripts/PlayRandomSound.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayRandomSound : MonoBehaviour
+{
+    public float frequency = 15;
+    public float randomness = 3;
+
+    public AudioClip[] allClips;
+
+    AudioSource source;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    IEnumerator PlayClips()
+    {
+        yield return new WaitForSeconds(frequency + Random.Range(-randomness, randomness));
+
+        source.PlayOneShot(allClips[Random.Range(0, allClips.Length)]);
+    }
+}
